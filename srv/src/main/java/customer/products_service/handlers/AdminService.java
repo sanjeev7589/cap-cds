c7d9cc5289cb16989bd18717afc7d50f3d40b061
sanjeev7589 <sanjeev.ngm7589@gmail.com>
2025-12-23 23:04:34 +0530

    chages on admin service and yaml

--- srv/src/main/java/customer/products_service/handlers/AdminService.java
+++ srv/src/main/java/customer/products_service/handlers/AdminService.java
@@ -3,6 +3,7 @@ package customer.products_service.handlers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.sap.cds.services.cds.CdsCreateEventContext;
@@ -11,16 +12,25 @@ import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.persistence.PersistenceService;
 
 @Component
 @ServiceName("AdminService")
 public class AdminService implements EventHandler {
-
+    @Autowired
+    PersistenceService persistenceService;
     private Map<Object, Map<String, Object>> products = new HashMap<>();
 
     @On(event = CqnService.EVENT_CREATE, entity = "AdminService.Products")
     public void onCreate(CdsCreateEventContext context) {
-        context.getCqn().entries().forEach(e -> products.put(e.get("ID"), e));
+        /*
+         * context.getCqn().entries().forEach(e -> products.put(e.get("ID"), e));
+         * context.setResult(context.getCqn().entries());
+         */
+        // Persist to DB
+        persistenceService.run(context.getCqn());
+
+        // Return result
         context.setResult(context.getCqn().entries());
     }
 
